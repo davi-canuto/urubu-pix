8d859e51b019b0d5a8293d314d1d11c21a5e85d6
davi-canuto <davicanutogregorio@gmail.com>
2023-06-11 19:00:02 -0300

    fix: fixes gitignore and refactor user spec

---
+++ db/migrate/20230611175101_create_orders.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateOrders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orders do |t|
+      t.float :value
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
--- spec/app/model/user_spec.rb
+++ spec/app/model/user_spec.rb
@@ -5,7 +5,7 @@ require './app/model/user'
 
 RSpec.describe User do
   context 'with user' do
-    let(:user) { User.new(name: 'John') }
+    let(:user) { described_class.new(name: 'John') }
 
     it { expect(user.name).to eq('John') }
   end
